a3758d0cb7b84ad61a6ddbd43335f90eff8cc277
Michael van Eerd <mveerd@vcn.nl>
2020-03-16 14:33:41 +0100

    Add current xrandr bootstrap for work pc at home

--- init/xrandr.sh
+++ init/xrandr.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --mode 1920x1200 --pos 0x760 --rotate left --output HDMI-0 --mode 1920x1080 --pos 1984x0 --rotate inverted --output DVI-I-1 --off --output DVI-I-0 --off --output DP-1 --off --output DP-0 --mode 3440x1440 --pos 1200x1080 --rotate normal
+xrandr --output VIRTUAL1 --off --output DP3 --mode 1920x1200 --pos 0x808 --rotate left --output DP2 --mode 3440x1440 --pos 1200x1080 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 2000x0 --rotate inverted

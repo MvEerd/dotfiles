3fe852ccd11da5646fddea3651557be345ff38cd
Michael van Eerd <mveerd@vcn.nl>
2020-07-08 15:14:46 +0200

    Modify xrandr.sh for work monitors again

--- init/xrandr.sh
+++ init/xrandr.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --mode 1920x1200 --pos 0x808 --rotate left --output DP2 --mode 3440x1440 --pos 1200x1080 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 2000x0 --rotate inverted
+xrandr --output DP1 --primary --mode 1920x1200 --pos 1200x360 --rotate normal --output DP2 --mode 1920x1200 --pos 0x0 --rotate left --output DP3 --mode 1920x1200 --pos 3120x360 --rotate normal --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off

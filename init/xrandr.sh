658f4a7f3d4380a7288af0cb512486152f08fc3b
Michael van Eerd <mveerd@vcn.nl>
2021-05-14 16:28:25 +0200

    .zsh, wpg, rofi, update gitignore

--- init/xrandr.sh
+++ init/xrandr.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP1 --primary --mode 1920x1200 --pos 1200x360 --rotate normal --output DP2 --mode 1920x1200 --pos 0x0 --rotate left --output DP3 --mode 1920x1200 --pos 3120x360 --rotate normal --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off
+xrandr --output DP1 --primary --mode 1920x1200 --pos 1920x360 --rotate normal --output DP2 --mode 1920x1200 --pos 3840x0 --rotate left --output DP3 --mode 1920x1200 --pos 0x360 --rotate normal --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off
